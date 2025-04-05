68f518c2a69ae57987768af7dcbaa6d100c313c2
Leandro Galuzzi <leandrogaluzzi@Leandros-MacBook-Air.local>
2025-04-05 16:26:40 +0200

    More cases

--- GalaxyFarFarAway/GalaxyFarFarAway/Model/Domain/Climate.swift
+++ GalaxyFarFarAway/GalaxyFarFarAway/Model/Domain/Climate.swift
@@ -2,8 +2,13 @@ import Foundation
 
 enum Climate: String {
     case arid
+    case frigid
     case frozen
+    case hot
+    case humid
+    case moist
     case murky
     case temperate
     case tropical
+    case windy
 }
--- GalaxyFarFarAway/GalaxyFarFarAway/Model/Domain/Terrain.swift
+++ GalaxyFarFarAway/GalaxyFarFarAway/Model/Domain/Terrain.swift
@@ -2,10 +2,13 @@ import Foundation
 
 enum Terrain: String {
     case barren
+    case caves
     case cityscape
     case desert
+    case deserts
     case forests
     case gasGiant = "gas giant"
+    case grass
     case grasslands
     case grassyHills = "grassy hills"
     case iceCaves = "ice caves"
@@ -16,11 +19,15 @@ enum Terrain: String {
     case mountain
     case mountains
     case ocean
+    case oceans
+    case plains
     case rainforests
+    case rivers
     case rock
     case savanna
     case swamp
     case swamps
     case tundra
+    case urban
     case windy
 }
